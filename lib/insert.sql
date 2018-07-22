5867c37dd578b5c3f5cf17e0c3b377d7b30ce0ea
Romeo Gono <romeogono1@aol.com>
2018-07-22 01:46:11 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (3, "Boxer", "Never Give Up", "Morgan", 1, 1), (4, "Benjamin", "Got to do this", "Equus", 1, 1);
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character1", "Think and Grow Rich", "Species A", 2, 2), (6, "Character2", "We Can Do This", "SpeciesB", 2, 2);
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (7,  "Character3", "You Can Do Great Things", "SpeciesC", 2, 2), (8, "Character4", "What a Genius", "SpeciesD", 2, 2);
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 2, 3), (2, 1, 1), (3, 2, 3), (4, 3, 4), (5, 1, 6), (6, 2, 2), (7, 4, 6), (8, 5, 5);
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 2, 1), (10, 1, 4), (11, 2, 6), (12, 5, 4), (13, 6, 6), (14, 6, 2), (15, 4, 6), (16, 5, 8);
